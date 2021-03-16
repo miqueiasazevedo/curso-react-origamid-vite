e4a7257d19863d8d16047f39fdd676306468f670
Miqueias Azevedo <miqueias.azevedo@hotmail.com>
2021-03-16 20:52:03 -0300

    First commit

---
+++ project-with-npm-MANUAL/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+
+// function App(){
+//     return React.createElement(
+//         'a', 
+//         {href: 'https://www.google.com'},
+//         'Google'
+//     );
+// }
+
+ReactDOM.render(<App />, document.getElementById('root')); 
