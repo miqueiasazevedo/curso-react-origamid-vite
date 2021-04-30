bd4347a282088ef0edfe1b9078fbd5e33d43c35a
Miqueias Azevedo <miqueias.azevedo@hotmail.com>
2021-04-30 01:39:39 +0000

    Refatoraçao para separaçao do fetch e aleteraçao de estados no ciclo de vida do componente

--- npx-create-react-app/meuapp/src/LoadPreferenceProductUseEfect/ProductItem.js
+++ npx-create-react-app/meuapp/src/LoadPreferenceProductUseEfect/ProductItem.js
@@ -1,10 +1,27 @@
 import React from "react";
 
 const ProductItem = ({ product }) => {
+  const [dadosProduct, setdadosProduct] = React.useState(null);
+
+  React.useEffect(() => {
+    if (product) fetchProduct(product);
+  }, [product]);
+
+  async function fetchProduct(productName) {
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${productName}`
+    );
+
+    const json = await response.json();
+
+    setdadosProduct(json);
+  }
+
+  if (dadosProduct === null) return null;
   return (
     <>
-      <h3>{product.nome}</h3>
-      <p>Preço: {product.preco}</p>
+      <h3>{dadosProduct.nome}</h3>
+      <p>Preço: R${dadosProduct.preco}</p>
     </>
   );
 };
