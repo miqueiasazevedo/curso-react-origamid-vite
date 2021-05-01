4b574f9f7969e5ed2eff70c845990a3aa1985167
Miqueias Azevedo <miqueias.azevedo@hotmail.com>
2021-05-01 02:53:01 +0000

    Criando o compartilhamento de estado dos produtos atraves do contexto

---
+++ npx-create-react-app/meuapp/src/LoadProductWithUseContext/GlobalContext.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const GlobalContext = React.createContext();
+
+export const GlobalStorage = ({ children }) => {
+  const [Products, setProducts] = React.useState(null);
+
+  async function fetchProducts() {
+    let products = await fetch(
+      "https://ranekapi.origamid.dev/json/api/produto/"
+    );
+
+    let json = await products.json();
+
+    setProducts(json);
+  }
+
+  function clearProducts() {
+    setProducts(null);
+  }
+
+  React.useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <GlobalContext.Provider value={{ Products, clearProducts }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
