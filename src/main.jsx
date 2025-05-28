17c4940b17d646757aa740fd810d329c27ef3de6
Miquéias Azevedo <43538720+miqueiasazevedo@users.noreply.github.com>
2025-05-28 18:32:45 +0000

    Instala e configura o tailwindCss com Vite e adiciona estilos globais

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+import './global.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
